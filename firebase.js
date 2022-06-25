30648aeeb7612592c1e4b7de8adf493588126aa3
System Administrator <root@Macs-MacBook-Pro.local>
2022-06-25 16:45:02 +0530

    freely commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB-D5RccUhL2axpbpf5Xa0RuyuyoZMP2eI",
+    authDomain: "freely-925d3.firebaseapp.com",
+    projectId: "freely-925d3",
+    storageBucket: "freely-925d3.appspot.com",
+    messagingSenderId: "245791870340",
+    appId: "1:245791870340:web:02513407760263c4bea1d1",
+    measurementId: "G-LGL6V35GTJ"
+  };
